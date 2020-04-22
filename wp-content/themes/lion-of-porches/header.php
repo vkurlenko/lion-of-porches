714803a94ec162674751c7b015cf458b29bc69ce
vkurlenko <vkurlenko@yandex.ru>
2020-04-22 23:30:47 +0300

    Верстка шапки и подвала

--- wp-content/themes/lion-of-porches/header.php
+++ wp-content/themes/lion-of-porches/header.php
@@ -2,57 +2,102 @@
 /**
  * Шаблон шапки (header.php)
  * @package WordPress
- * @subpackage your-clean-template-3
+ * @subpackage LionOfPorches
  */
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); // вывод атрибутов языка ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); // кодировка ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?php bloginfo( 'charset' ); // кодировка ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php /* RSS и всякое */ ?>
-	<link rel="alternate" type="application/rdf+xml" title="RDF mapping" href="<?php bloginfo('rdf_url'); ?>">
-	<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rss_url'); ?>">
-	<link rel="alternate" type="application/rss+xml" title="Comments RSS" href="<?php bloginfo('comments_rss2_url'); ?>">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php /* RSS и всякое */ ?>
+    <link rel="alternate" type="application/rdf+xml" title="RDF mapping" href="<?php bloginfo('rdf_url'); ?>">
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rss_url'); ?>">
+    <link rel="alternate" type="application/rss+xml" title="Comments RSS" href="<?php bloginfo('comments_rss2_url'); ?>">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-	<?php /* Все скрипты и стили теперь подключаются в functions.php */ ?>
+    <link rel="stylesheet" href="/wp-content/themes/lion-of-porches/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <?php /* Все скрипты и стили теперь подключаются в functions.php */ ?>
 
-	<!--[if lt IE 9]>
-	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	
-	<?php wp_head(); // необходимо для работы плагинов и функционала ?>
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <?php wp_head(); // необходимо для работы плагинов и функционала ?>
 </head>
+
 <body <?php body_class(); // все классы для body ?>>
-	<header>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<nav class="navbar navbar-default">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topnav" aria-expanded="false">
-								<span class="sr-only">Меню</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-						</div>
-						<div class="collapse navbar-collapse" id="topnav">
-							<?php $args = array( // опции для вывода верхнего меню, чтобы они работали, меню должно быть создано в админке
-								'theme_location' => 'top', // идентификатор меню, определен в register_nav_menus() в functions.php
-								'container'=> false, // обертка списка, тут не нужна
-						  		'menu_id' => 'top-nav-ul', // id для ul
-						  		'items_wrap' => '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>',
-								'menu_class' => 'top-menu', // класс для ul, первые 2 обязательны
-						  		'walker' => new bootstrap_menu(true) // верхнее меню выводится по разметке бутсрапа, см класс в functions.php, если по наведению субменю не раскрывать то передайте false		  		
-					  			);
-								wp_nav_menu($args); // выводим верхнее меню
-							?>
-						</div>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</header>
+<header>
+    <div class="container">
+        <div class="row">
+
+            <!-- top-left-block -->
+            <div class="col-md-5 col-sm-5 col-xs-5 header-left hidden-xs" style="/*background: #ccc*/">
+                <div class="main-menu">
+                    <ul>
+                        <li><a href="#">Женщины</a></li><li><a href="#">Мужчины</a></li><li><a href="#">Девочки</a></li><li><a href="#">Мальчики</a></li>
+                    </ul>
+                </div>
+
+                <div class="open-search">
+                    <form class="form-inline">
+                        <!--<input class="form-control" type="text" value="" placeholder="поиск">
+                        <i class="fa fa-search" aria-hidden="true"></i>-->
+
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="" placeholder="поиск">
+                            <div class="input-group-addon"><button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button></div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+            <!-- /top-left-block -->
+
+            <!-- logo -->
+            <div class="col-md-2 col-sm-2 col-xs-6 logo">
+                <h1>
+                    <a href="#"><img class="logo-red hidden-xs" src="/wp-content/themes/lion-of-porches/img/lion-of-porches.png"><img class="logo-blue visible-xs" src="/wp-content/themes/lion-of-porches/img/lion-of-porches-blue.webp"></a>
+                </h1>
+            </div>
+            <!-- /logo -->
+
+            <!-- top-right-block -->
+            <div class="col-md-5  col-sm-5  col-xs-6 header-right"  style="/*background: #00ff00*/">
+                <div class="top-menu-1 hidden-xs">
+                    <ul>
+                        <li><a href="#">Доставка</a></li><li><a href="#">О компании</a> </li>
+                    </ul>
+                </div>
+                <div class="top-menu-2 hidden-xs">
+                    <ul>
+                        <li><a href="#">Каталоги</a></li><li><a href="#">Партнерам</a></li><li><a href="#">Войти</a></li><li><a class="cart-link" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span><span class="count">(0)</span></a></li>
+                    </ul>
+                </div>
+
+                <div class="cart-mobile visible-xs">
+                    <div>
+                        <a class="cart-link" href="#" ><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span><span class="count">(0)</span></a>
+                        <div class='threebar hamburger'>
+                            <div class='bar'></div>
+                            <div class='bar'></div>
+                            <div class='bar'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /top-right-block -->
+        </div>
+
+        <!-- mobile menu -->
+        <?php get_template_part( 'parts/main-menu-mobile');?>
+        <!-- /mobile menu -->
+    </div>
+</header>
+
+<div class="main container">
+    <div class="row">
+        text
+    </div>
+</div>
---
+++ wp-content/themes/lion-of-porches/parts/main-menu-mobile.php
@@ -0,0 +1,30 @@
+<div class="row visible-xs">
+    <div class="main-menu mobile hidden">
+
+        <ul>
+            <li><a href="#">Женщины</a></li><li><a href="#">Мужчины</a></li><li><a href="#">Девочки</a></li><li><a href="#">Мальчики</a></li>
+        </ul>
+
+        <form>
+            <div class="row open-search mobile">
+                <div class="col-xs-6"><input type="text" value="" placeholder="поиск"></div>
+                <div class="col-xs-6  btn-search"><input type="submit" value="Найти" class="btn btn-primary btn-submit"></div>
+                <!--<span class="glyphicon glyphicon-search" aria-hidden="true"></span>-->
+            </div>
+        </form>
+
+        <div class="row">
+            <div class="col-xs-6">
+                <ul>
+                    <li><a href="#">Доставка</a></li><li><a href="#">О компании</a> </li>
+                </ul>
+            </div>
+            <div class="col-xs-6">
+                <ul>
+                    <li><a href="#">Каталоги</a></li><li><a href="#">Партнерам</a></li><li><a href="#">Войти</a></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</div>
