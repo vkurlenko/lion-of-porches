3497927584f329624012894a665174da0e5e87da
vkurlenko <vkurlenko@yandex.ru>
2020-04-27 10:39:08 +0300

    db config

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define( 'DB_NAME', 'lip' );
+define( 'DB_NAME', 'vinci-2_lip' );
 
 /** Имя пользователя MySQL */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'vinci-2_mysql' );
 
 /** Пароль к базе данных MySQL */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'sbanfy3e' );
 
 /** Имя сервера MySQL */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'vinci-2.mysql' );
 
 /** Кодировка базы данных для создания таблиц. */
 define( 'DB_CHARSET', 'utf8' );
