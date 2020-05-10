99edf1aee5a10f83b5b9d872f7b939da68108e80
vkurlenko <vkurlenko@yandex.ru>
2020-05-10 10:48:09 +0300

    404 страница

--- wp-content/themes/lion-of-porches/404.php
+++ wp-content/themes/lion-of-porches/404.php
@@ -9,10 +9,10 @@ get_header(); // Подключаем header.php ?>
 	<div class="container">
 		<div class="row">
 			<div class="<?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?>">
-				<h1>Ой, это 404!</h1>
-				<p>Блаблабла 404 Блаблабла</p>
+				<h1>Страница не найдена</h1>
+				<!--<p>Блаблабла 404 Блаблабла</p>-->
 			</div>
-			<?php get_sidebar(); // подключаем sidebar.php ?>
+			<?php /*get_sidebar(); // подключаем sidebar.php */?>
 		</div>
 	</div>
 </section>
