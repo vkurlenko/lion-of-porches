b8bdff53f2322d6f48e95d9c335f1d115ae63d72
vkurlenko <vkurlenko@yandex.ru>
2020-05-18 20:58:18 +0300

    Фиксированное меню

--- wp-content/themes/lion-of-porches/js/main.js
+++ wp-content/themes/lion-of-porches/js/main.js
@@ -1,6 +1,7 @@
 // custom scripts
 $ = jQuery.noConflict();
 
+/* Мобильное меню (гамбургер) */
 $(document).on('click', '.hamburger', function() {
     $('.threebar')
         .removeClass('hamburger')
@@ -9,7 +10,7 @@ $(document).on('click', '.hamburger', function() {
 
 });
 
-// Arrow to Hamburger
+
 $(document).on('click', '.cross', function() {
     $('.threebar')
         .removeClass('cross')
@@ -20,6 +21,20 @@ $(document).on('click', '.cross', function() {
 function toggleMobileMenu() {
     $('.main-menu.mobile').toggleClass('hidden');
 }
+/* /Мобильное меню (гамбургер) */
+
+
+/* Фиксация главного меню при прокрутке страницы */
+$(window).on("scroll", function() {
+    if ($(window).scrollTop() > 115) {
+        $('header').addClass('fixed');
+    }
+    else {
+        $('header').removeClass('fixed');
+    }
+});
+/* /Фиксация главного меню при прокрутке страницы */
+
 
 $(document).ready(function() {
     /** слайдер Магазины */
@@ -90,3 +105,53 @@ $('.sub-menu').on('mouseleave', function() {
     $('li a.btn-alt').removeClass('active');
 
 })
+
+$(window).load(function() {
+    /*if($('.variations_form .woocommerce-Price-amount').length && $('.variations_form .woocommerce-Price-amount').text() != '') {
+        window.setTimeout(function(){
+            $('p.price .woocommerce-Price-amount').html($('.variations_form .woocommerce-Price-amount').html());
+        }, 0);
+    }*/
+
+    /*console.log($('.variations_form .discount-personal .woocommerce-Price-amount'));*/
+    //console.log($('.single_variation .woocommerce-variation-price').html());
+
+    if($('.single_variation .woocommerce-variation-price').html() == undefined || $('.single_variation .woocommerce-variation-price').html() == '') {
+          $('.summary > .discount-personal').css('display', 'block');
+    }
+
+    /*if($('.single_variation .woocommerce-variation-price').html() != undefined && $('.single_variation .woocommerce-variation-price').html() != '') {
+        $('.summary > .discount-personal').css('display', 'none');
+    }*/
+
+    //$('.variations_form select').trigger('click');
+
+    $('.variations_form select').change(function() {
+
+        //console.log('change');
+
+        if($('.variations_form .single_variation .woocommerce-Price-amount').length && $('.variations_form .single_variation .woocommerce-Price-amount').text() != '') {
+            window.setTimeout(function(){
+
+                var price = $('.variations_form .single_variation .woocommerce-Price-amount').html();
+                //console.log(price.replace(/(<[^>]+>)|(₽)|,/g,''));
+
+                price = price.replace(/(<[^>]+>)|(₽)|,/g,'') * 1;
+
+                //console.log(price);
+
+                var discount = $('.summary > .discount-personal > .discount-value > span').text();
+                discount = discount * 1 / 100;
+                //console.log(discount);
+
+                var discount_price = price - price * discount;
+                var s = price - discount_price;
+
+                $('.single_variation p.price .woocommerce-Price-amount').html('₽ '+discount_price);
+                $('.single_variation p.s .woocommerce-Price-amount').html('₽ '+s);
+
+                $('.woocommerce-variation-price .discount-personal, .woocommerce-variation-price .price').css('display', 'block');
+            }, 0);
+        }
+    });
+});
