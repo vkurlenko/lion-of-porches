5cbd21a132d3b79449539c1e1458f3725b1d25ef
vkurlenko <vkurlenko@yandex.ru>
2020-05-20 20:44:26 +0300

    Блок уровня пользователя

---
+++ wp-content/themes/lion-of-porches/parts/user-level-block.php
@@ -0,0 +1,19 @@
+<?php
+$current_user = wp_get_current_user();
+$user_discount = (new Crm())->getUserDiscount($current_user->user_email);
+
+if($user_discount) {
+    $user_discount_level = (new Crm())->getUserLevel()[$user_discount];
+    /*echo 'level='.$user_discount_level;
+    (new Helper())->dump($current_user);*/
+
+    ?>
+
+    <div class="user-discount-level">
+        <span class="user-name"><?=$current_user->display_name?></span>
+        <span class="user-level"><?=$user_discount_level?></span>
+        <img id="user-level-label" src="/wp-content/themes/lion-of-porches/img/levels/<?=strtolower($user_discount_level)?>.jpg">
+    </div>
+    <?php
+}
+?>
