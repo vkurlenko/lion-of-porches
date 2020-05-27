e597fa6763a094ad4f44adaabda1a4bf739c0335
vkurlenko <vkurlenko@yandex.ru>
2020-05-27 20:16:40 +0300

    Формат цены "от"

--- wp-content/themes/lion-of-porches/functions.php
+++ wp-content/themes/lion-of-porches/functions.php
@@ -545,6 +545,27 @@ function get_related_custom($id, $limit = 4 ) {
 /*************************************************************************************************************/
 /*************************************************************************************************************/
 
+add_filter( 'woocommerce_variable_sale_price_html', 'my_variation_price_format', 10, 2 );
+
+add_filter( 'woocommerce_variable_price_html', 'my_variation_price_format', 10, 2 );
+
+function my_variation_price_format( $price, $product ) {
+
+// Main Price
+    $prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
+    $price = $prices[0] !== $prices[1] ? sprintf( __( 'от %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+
+// Sale Price
+    $prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
+    sort( $prices );
+    $saleprice = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+
+    if ( $price !== $saleprice ) {
+        //$price = '<del>' . $saleprice . '</del> <ins>' . $price . '</ins>';
+    }
+    return $price;
+}
+
 //add_filter( 'woocommerce_product_get_sale_price', 'custom_dynamic_sale_price', 10, 2 );
 //add_filter( 'woocommerce_product_variation_get_sale_price', 'custom_dynamic_sale_price', 10, 2 );
 
