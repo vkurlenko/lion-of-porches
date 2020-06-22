b3c18bc023a2209f3d65b5a4aebc852f90122f00
vkurlenko <vkurlenko@yandex.ru>
2020-06-22 17:37:05 +0300

    test2

--- wp-content/themes/lion-of-porches/contact.php
+++ wp-content/themes/lion-of-porches/contact.php
@@ -20,7 +20,7 @@ get_header(); // подключаем header.php ?>
                         <div class="col-md-6 content">
                             <h2><?=$post->post_title;?></h2>
 
-                            <p><?=$post->post_content;?></p>
+                            <!--<p><?/*=$post->post_content;*/?></p>-->
 
                             <!--<img  class="hidden-xs" src="/wp-content/themes/lion-of-porches/img/Screenshot_1.jpg">-->
 
