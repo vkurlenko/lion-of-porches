3383f441af3bdac036e1f2e5e5d4fe033ab17e90
vkurlenko <vkurlenko@yandex.ru>
2020-06-22 18:55:30 +0300

    style

--- wp-content/themes/lion-of-porches/single.php
+++ wp-content/themes/lion-of-porches/single.php
@@ -18,7 +18,7 @@ get_header(); // подключаем header.php ?>
                 ?>
 
                 <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
+                    <div class="col-md-6 col-md-offset-3">
                         <h2><?=$post->post_title;?></h2>
                         <?=$post->post_content;?>
                     </div>
