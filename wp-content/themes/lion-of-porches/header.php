48f6e8f0678b00d8468fc0f190dfc39a571010c5
vkurlenko <vkurlenko@yandex.ru>
2020-09-02 18:49:32 +0300

    add pixelcode

--- wp-content/themes/lion-of-porches/header.php
+++ wp-content/themes/lion-of-porches/header.php
@@ -39,6 +39,26 @@ die;*/
     <![endif]-->
 
     <?php wp_head(); // необходимо для работы плагинов и функционала ?>
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '663993440893072');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1"
+             src="https://www.facebook.com/tr?id=663993440893072&ev=PageView
+&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
 </head>
 
 <body <?php body_class(); // все классы для body ?>>
