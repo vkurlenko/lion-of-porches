7b31a18dba5e40f7a0da0e1be8b2216decf96d60
vkurlenko <vkurlenko@yandex.ru>
2021-06-10 21:15:49 +0300

    add breadcrumb to category page

--- wp-content/themes/lion-of-porches/category.php
+++ wp-content/themes/lion-of-porches/category.php
@@ -7,14 +7,25 @@
 get_header(); // подключаем header.php ?> 
 <section>
 	<div class="container">
+
 		<div class="row">
-			<div class="<?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?>">
-				<h1><?php single_cat_title(); // название категории ?></h1>
-				<?php if (have_posts()) : while (have_posts()) : the_post(); // если посты есть - запускаем цикл wp ?>
-					<?php get_template_part('loop'); // для отображения каждой записи берем шаблон loop.php ?>
-				<?php endwhile; // конец цикла
-				else: echo '<p>Нет записей.</p>'; endif; // если записей нет, напишим "простите" ?>	 
-				<?php pagination(); // пагинация, функция нах-ся в function.php ?>
+
+			<div class="<?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?> col-md-12 content">
+                <div class="row">
+                    <div class="col-md-12 woocommerce-breadcrumb" >
+                        <?php if( function_exists('kama_breadcrumbs') ) kama_breadcrumbs('<span class="breadcrumb-delimiter">|</span>'); ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3" >
+                    <h1><?php single_cat_title(); // название категории ?></h1>
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); // если посты есть - запускаем цикл wp ?>
+                        <?php get_template_part('loop'); // для отображения каждой записи берем шаблон loop.php ?>
+                    <?php endwhile; // конец цикла
+                    else: echo '<p>Нет записей.</p>'; endif; // если записей нет, напишим "простите" ?>
+                    <?php pagination(); // пагинация, функция нах-ся в function.php ?>
+                </div>
+
 			</div>
 			<?php //get_sidebar(); // подключаем sidebar.php ?>
 		</div>
